7f59fec97e991d486cc519cb15d2f1ac36e2ba98
Martin Ondrej <martin@salstar.sk>
2022-05-03 19:55:46 +0200

    player animation for 4 directions and other 4 only arrows. FIRST RELESE

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -1,47 +1,96 @@
 extends KinematicBody2D
 
-var direction_of_sprite_1 = preload("res://assets/player/player_idle_pos_1.png")
-var direction_of_sprite_2 = preload("res://assets/player/player_idle_pos_2.png")
-var direction_of_sprite_3 = preload("res://assets/player/player_idle_pos_3.png")
-var direction_of_sprite_4 = preload("res://assets/player/player_idle_pos_4.png")
-var direction_of_sprite_5 = preload("res://assets/player/player_idle_pos_5.png")
-var direction_of_sprite_6 = preload("res://assets/player/player_idle_pos_6.png")
-var direction_of_sprite_7 = preload("res://assets/player/player_idle_pos_7.png")
-var direction_of_sprite_8 = preload("res://assets/player/player_idle_pos_8.png")
-onready var player_state = "idle"
+onready var player_state: String = "idle"
+onready var player_direction: int = 1
 
 var velocity = Vector2.ZERO
 
-onready var player_sprite = get_node("player_idle_pos_1")
+onready var player_anim_sprite = get_node("player_anim")
+
+export var speed : int = 100
+
 func _physics_process(delta):
+	print(delta*speed)
 	if (Input.is_action_pressed("right") 
 	or Input.is_action_pressed("left") 
 	or Input.is_action_pressed("up") 
 	or Input.is_action_pressed("down")):
 		move_and_slide(velocity)
-	if (Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_1)
-		velocity = Vector2(100, 0)
-	elif (not Input.is_action_pressed("right") and Input.is_action_pressed("left") and not Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_5)
-		velocity = Vector2(-100, 0)
-	elif (not Input.is_action_pressed("right") and not Input.is_action_pressed("left") and Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_7)
-		velocity = Vector2(0, -100)
-	elif (not Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not Input.is_action_pressed("up") and Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_3)
-		velocity = Vector2(0, 100)
-	elif (Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not Input.is_action_pressed("up") and Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_2)
-		velocity = Vector2(100, 100)
-	elif (Input.is_action_pressed("right") and not Input.is_action_pressed("left") and Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_8)
-		velocity = Vector2(100, -100)
-	elif (not Input.is_action_pressed("right") and Input.is_action_pressed("left") and Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_6)
-		velocity = Vector2(-100, -100)
-	elif (not Input.is_action_pressed("right") and Input.is_action_pressed("left") and not Input.is_action_pressed("up") and Input.is_action_pressed("down")):
-		player_sprite.set_texture(direction_of_sprite_4)
-		velocity = Vector2(-100, 100)
-		
+		player_state = "walking"
+	else:
+		player_state = "idle"
 
+	if (Input.is_action_pressed("right") 
+	and not Input.is_action_pressed("left") 
+	and not Input.is_action_pressed("up") 
+	and not Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_1")
+		velocity = Vector2(speed*delta, 0)
+		player_direction = 1
+	elif (not Input.is_action_pressed("right") 
+	and Input.is_action_pressed("left") 
+	and not Input.is_action_pressed("up") 
+	and not Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_5")
+		velocity = Vector2(-speed*delta, 0)
+		player_direction = 5
+	elif (not Input.is_action_pressed("right") 
+	and not Input.is_action_pressed("left") 
+	and Input.is_action_pressed("up") 
+	and not Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_7")
+		velocity = Vector2(0, -speed*delta)
+		player_direction = 7
+	elif (not Input.is_action_pressed("right") 
+	and not Input.is_action_pressed("left") 
+	and not Input.is_action_pressed("up") 
+	and Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_3")
+		velocity = Vector2(0, speed*delta)
+		player_direction = 3
+	elif (Input.is_action_pressed("right") 
+	and not Input.is_action_pressed("left") 
+	and not Input.is_action_pressed("up") 
+	and Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_2")
+		velocity = Vector2(speed*delta, speed*delta)
+		player_direction = 2
+	elif (Input.is_action_pressed("right") 
+	and not Input.is_action_pressed("left") 
+	and Input.is_action_pressed("up") 
+	and not Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_8")
+		velocity = Vector2(speed*delta, -speed*delta)
+		player_direction = 8
+	elif (not Input.is_action_pressed("right") 
+		and Input.is_action_pressed("left") 
+		and Input.is_action_pressed("up") 
+		and not Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_6")
+		velocity = Vector2(-speed*delta, -speed*delta)
+		player_direction = 6
+	elif (not Input.is_action_pressed("right") 
+		and Input.is_action_pressed("left") 
+		and not Input.is_action_pressed("up") 
+		and Input.is_action_pressed("down")):
+		player_anim_sprite.play("walking_pos_4")
+		velocity = Vector2(-speed*delta, speed*delta)
+		player_direction = 4
+	if (player_state == "idle"):
+		if (player_direction == 1):
+			player_anim_sprite.play("idle_pos_1")
+		elif (player_direction == 2):
+			player_anim_sprite.play("idle_pos_2")
+		elif (player_direction == 3):
+			player_anim_sprite.play("idle_pos_3")
+		elif (player_direction == 4):
+			player_anim_sprite.play("idle_pos_4")
+		elif (player_direction == 5):
+			player_anim_sprite.play("idle_pos_5")
+		elif (player_direction == 6):
+			player_anim_sprite.play("idle_pos_6")
+		elif (player_direction == 7):
+			player_anim_sprite.play("idle_pos_7")
+		elif (player_direction == 8):
+			player_anim_sprite.play("idle_pos_8")
+	
