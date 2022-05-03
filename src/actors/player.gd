23607230c47ee62f3f64d99c26ecb22fe13dbe32
Martin Ondrej <martin@salstar.sk>
2022-05-03 17:32:58 +0200

    player movement with textures 1-8, Only 2&5 textures done. 1, 3, 4, 6, 7, 8 only programmer textures

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
 var direction_of_sprite_1 = preload("res://assets/player/player_idle_pos_1.png")
-var direction_of_sprite_2 = preload("res://assets/player/player_idle_pos_1.png")
-var direction_of_sprite_3 = preload("res://assets/player/player_idle_pos_1.png")
-var direction_of_sprite_4 = preload("res://assets/player/player_idle_pos_1.png")
+var direction_of_sprite_2 = preload("res://assets/player/player_idle_pos_2.png")
+var direction_of_sprite_3 = preload("res://assets/player/player_idle_pos_3.png")
+var direction_of_sprite_4 = preload("res://assets/player/player_idle_pos_4.png")
 var direction_of_sprite_5 = preload("res://assets/player/player_idle_pos_5.png")
-var direction_of_sprite_6 = preload("res://assets/player/player_idle_pos_5.png")
-var direction_of_sprite_7 = preload("res://assets/player/player_idle_pos_5.png")
-var direction_of_sprite_8 = preload("res://assets/player/player_idle_pos_5.png")
+var direction_of_sprite_6 = preload("res://assets/player/player_idle_pos_6.png")
+var direction_of_sprite_7 = preload("res://assets/player/player_idle_pos_7.png")
+var direction_of_sprite_8 = preload("res://assets/player/player_idle_pos_8.png")
 onready var player_state = "idle"
 
 var velocity = Vector2.ZERO
@@ -31,5 +31,17 @@ func _physics_process(delta):
 	elif (not Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not Input.is_action_pressed("up") and Input.is_action_pressed("down")):
 		player_sprite.set_texture(direction_of_sprite_3)
 		velocity = Vector2(0, 100)
+	elif (Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not Input.is_action_pressed("up") and Input.is_action_pressed("down")):
+		player_sprite.set_texture(direction_of_sprite_2)
+		velocity = Vector2(100, 100)
+	elif (Input.is_action_pressed("right") and not Input.is_action_pressed("left") and Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
+		player_sprite.set_texture(direction_of_sprite_8)
+		velocity = Vector2(100, -100)
+	elif (not Input.is_action_pressed("right") and Input.is_action_pressed("left") and Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
+		player_sprite.set_texture(direction_of_sprite_6)
+		velocity = Vector2(-100, -100)
+	elif (not Input.is_action_pressed("right") and Input.is_action_pressed("left") and not Input.is_action_pressed("up") and Input.is_action_pressed("down")):
+		player_sprite.set_texture(direction_of_sprite_4)
+		velocity = Vector2(-100, 100)
 		
 
